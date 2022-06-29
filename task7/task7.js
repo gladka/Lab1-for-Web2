349bf123c7c179d5059e8fc93fddbf862c61b3eb
Kateryna Gladka <pi60_gkv@student.ztu.edu.ua>
2022-06-29 16:00:42 +0300

    removed reducant code and modified search for LastName and FirstName. Fixed bug in Email search.

--- task7/task7.js
+++ task7/task7.js
@@ -3,17 +3,13 @@ buttonDataTable.addEventListener('click',
     function createTable(event) {
         event.preventDefault();
         let tableDataText = document.querySelector('#tableDataText');
-        let resDataTable = document.querySelector('#resDataTable');
 
         let td = document.querySelector('#td');
         td.style.border = '1px solid black';
-
         let test;
-        let index = 0;
         let part;
-        let array = (tableDataText.value).split(/\n/gm);
 
-        let regFirstName = /^([A-Z][a-z]+?) ([A-Z][a-z]+)$/gm;
+        let regFirstName = /^([A-Z][a-z]+?) ([A-Z][a-z]+)$\n/gm;
         let FirstNameLabel = document.querySelector('#FirstNameLabel');
         FirstNameLabel.innerHTML = 'FirstName: ';
         let FirstName = document.querySelector('#FirstName');
@@ -21,7 +17,7 @@ buttonDataTable.addEventListener('click',
         let LastNameLabel = document.querySelector('#LastNameLabel');
         LastNameLabel.innerHTML = 'LastName: ';
         let LastName = document.querySelector('#LastName');
-        while (test = regFirstName.exec(array[index++])) {
+        while (test = regFirstName.exec(tableDataText.value)) {
             part = test[0].split(' ');
             FirstName.innerHTML = part[0];
             LastName.innerHTML = part[1];
@@ -69,7 +65,7 @@ buttonDataTable.addEventListener('click',
                 Password.innerHTML=test[0];
         }
 
-        let regEmail=/^([\d\w]+?)?@([\d\w]+?)?.(\w+?)\n/gm;
+        let regEmail=/^([\d\w]+?)?@([\d\w]+?)?\.(\w+?)$/gm;
         let EmailLabel=document.querySelector('#EmailLabel');
         EmailLabel.innerHTML='Email: ';
         let Email= document.querySelector('#Email');
