edd2e2043d3ba7d203eb53139aeb84c2e6190f22
Kateryna Gladka <pi60_gkv@student.ztu.edu.ua>
2022-06-29 11:08:12 +0300

    Сreated folder task7 and 2 task7.html and task7.js files. in the task7.html file, a form and a blank for the future table are created. in the task7.js file, a button event is created and part of the createТable function is implemented. Тhe function created a regular expression on the field firstname

---
+++ task7/task7.js
@@ -0,0 +1,25 @@
+let buttonDataTable = document.querySelector('#buttonDataTable');
+buttonDataTable.addEventListener('click',
+    function createTable(event){
+    event.preventDefault();
+    let tableDataText = document.querySelector('#tableDataText');
+    let resDataTable = document.querySelector('#resDataTable');
+
+    let td=document.querySelector('#td');
+    td.style.border='1px solid black';
+
+    let test;
+    let index=0;
+    let part;
+        let array=(tableDataText.value).split(/\n/gm);
+
+        let regFirstName=/^([A-Z][a-z]+?) ([A-Z][a-z]+)$/gm;
+        let FirstNameLabel=document.querySelector('#FirstNameLabel');
+        FirstNameLabel.innerHTML='FirstName: ';
+        let FirstName=document.querySelector('#FirstName');
+        while(test=regFirstName.exec(array[index++])){
+            part=test[0].split(' ');
+            FirstName.innerHTML=part[0];
+        }
+    }
+);
